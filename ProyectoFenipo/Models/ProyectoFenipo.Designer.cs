a740da28707b10fe62024cf1638e3539026e8605
TotoroBy <flasherx99@gmail.com>
2022-11-30 17:27:29 -0600

    No se, probando?

--- ProyectoFenipo/Models/ProyectoFenipo.Designer.cs
+++ ProyectoFenipo/Models/ProyectoFenipo.Designer.cs
@@ -1,4 +1,4 @@
-﻿// La generación de código T4 está habilitada para el modelo 'C:\Users\Uriel Sandi G\Desktop\ProyectoFenipo\ProyectoFenipo\Models\ProyectoFenipo.edmx'. 
+﻿// La generación de código T4 está habilitada para el modelo 'C:\Users\asd\source\repos\ProyectoFenipo\ProyectoFenipo\Models\ProyectoFenipo.edmx'. 
 // Para habilitar la generación de código heredada, cambie el valor de la propiedad del diseñador 'Estrategia de generación de código'
 // por 'ObjectContext heredado'. Esta propiedad está disponible en la ventana Propiedades cuando se abre
 // el modelo en el diseñador.
